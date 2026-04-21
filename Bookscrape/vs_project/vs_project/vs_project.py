7298a4d5339f427c5ca5d8d7650ac2e290a48d0e
acidic <aiden.l.jones10@gmail.com>
2026-04-21 13:22:07 -0700

    Uploaded code for vs_project

---
+++ Bookscrape/vs_project/vs_project/vs_project.py
@@ -0,0 +1,9 @@
+# I am organizing my code in a certain way. Don't fuck with my code
+
+class App():
+    def __init__():
+        pass
+
+if __name__ == "__main__":
+    application = App()
+    # main loop
