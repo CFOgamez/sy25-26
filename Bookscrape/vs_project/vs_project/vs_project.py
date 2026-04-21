0f06691b892b6789026c957bc4e8a5bd30132077
acidic <aiden.l.jones10@gmail.com>
2026-04-21 13:40:59 -0700

    ensured website can be accessed

--- Bookscrape/vs_project/vs_project/vs_project.py
+++ Bookscrape/vs_project/vs_project/vs_project.py
@@ -1,8 +1,23 @@
 # I am organizing my code in a certain way. Don't fuck with my code
+import requests
+from bs4 import BeautifulSoup
 
 class App():
-    def __init__():
-        pass
+    def __init__(self):
+        # Start application
+        self.load_bookscrape()
+
+    def interface(self):
+        print("Load interface")
+
+    def load_bookscrape(self):
+        URL = "https://books.toscrape.com/"
+        response = requests.get(URL)
+        
+        if response.status_code == 200:
+            print("Request to Bookscrape successful")
+        else:
+            print(f"Request failed with status code: {response.status_code}")
 
 if __name__ == "__main__":
     application = App()
